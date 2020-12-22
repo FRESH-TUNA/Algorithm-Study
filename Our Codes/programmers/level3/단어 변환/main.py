ce1f6d1ca9f3c9e7ed4192f85b5ce8db0ec2b351
lunacircle4 <lunacircle4@likelion.org>
2020-12-22 22:00:45 +0900

    feat: 너무 어렵군

--- Our Codes/programmers/level3/단어 변환/main.py
+++ Our Codes/programmers/level3/단어 변환/main.py
@@ -3,7 +3,7 @@ def solution(begin, target, words):
         def __init__(self, begin, target, words):
             self.begin = begin
             self.target = target
-            self.words = words
+            self.words = [begin] + words
             self.answer = 100
             self.depth = 0
             self.is_traced = [False for _ in range(len(words))]
@@ -11,32 +11,24 @@ def solution(begin, target, words):
         def check(self, i, j):
             count = 0
             for z in range(len(i)):
-                if i[z] != j[z]:
-                    count += 1
-                if count > 1:
-                    return False
+                if i[z] != j[z]: count += 1
+                if count > 1: return False
             return True
 
         def dfs(self, i):
-            self.is_traced[i] = True
-            self.depth += 1
-        
             if self.words[i] == self.target:
-                self.depth = min(self.depth, self.answer)
+                self.answer = min(self.depth, self.answer)
             else:    
                 for y, word in enumerate(self.words):
-                    if self.is_traced[y] and self.check(self.words[i], word):
+                    if not self.is_traced[y] and self.check(self.words[i], word):
+                        self.is_traced[i] = True
+                        self.depth += 1
                         self.dfs(y)
-        
-            self.is_traced[i] = False
-            self.depth -= 0
          
         def service(self):
-            if target not in words: return 0
-            for i, word in enumerate(words):
-                if self.check(self.begin, word):
-                    self.dfs(i)
-            return answer if answer != 100 else 0
+            if target not in self.words: return 0
+            self.dfs(0)
+            return self.answer if self.answer != 100 else 0
         
     return Solution(begin, target, words).service()
         
