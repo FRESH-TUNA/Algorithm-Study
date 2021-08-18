ea0b7ef24ef31901976d5f75c2d30d1558f25d48
lunacircle4 <lunacircle4@likelion.org>
2021-08-19 02:03:38 +0900

    feat: bfs 로 거리두기 문제 풀기

--- Our Codes/programmers/level2/거리두기 확인하기/main2.py
+++ Our Codes/programmers/level2/거리두기 확인하기/main2.py
@@ -1,50 +1,83 @@
-def solution(places: List[List]):
+from collections import deque
 
-    def a_place(place):
-        '하나의 대기실 체크'
-        # 문제 생기면 바로 return 0
-        ans = 1
-        def build_visited():
-            nonlocal place
-            visited_map = [[False for i in range(5)] for j in range(5)]
-            for i in range(5):
-                for j in range(5):
-                    if place[i][j] == 'X':
-                        visited_map[i][j] = True
-            return visited_map
-            
+def check(stack):
+    if stack[0] == "O" and stack[1] == "O": return True
+    return False
 
-        def dfs_helper(root, distance, first_check):
-            nonlocal place, visited_map, ans
-            i, j = root[0], root[1]
-            visited_map[i][j] = True
-            if place[i][j] == 'P' and first_check != 0:
-                if distance <= 2:
-                    ans = 0
-                return
-            
-            togo = [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]
-            def check_loc(loc):
-                nonlocal visited_map
-                i, j = loc[0], loc[1]
-                if i < 0 or i > 4: return False
-                if j < 0 or j > 4: return False
-                if visited_map[i][j]:
-                    return False
-                return loc
-            togo = list(map(check_loc, togo))
+def out_of_range(_case, case):
+    if _case[0] < len(case) and _case[1] < len(case) and _case[0] >= 0 and _case[1] >= 0:
+        return False
+    return True
 
-            for loc in togo:
-                if loc:
-                    dfs_helper(loc, distance+1, 1)
-        
-        for i in range(5):
-            for j in range(5):
-                if place[i][j] == 'P':
-                    visited_map = build_visited()
-                    dfs_helper((i,j), 0, 0)
-                    if ans == 0:
-                        return 0
-        return 1
+def dfs(is_traced, stack, case, i, j):
+    if len(stack) == 2:
+        return check(stack)
+
+    _cases = [[i+1, j], [i, j+1], [i-1, j], [i, j-1]]
+    for _case in _cases:
+        if not out_of_range(_case, case) and not is_traced[_case[0]][_case[1]]:
+            if case[_case[0]][_case[1]] == "P":
+                return False
+            is_traced[_case[0]][_case[1]] = True
+            stack.append(case[_case[0]][_case[1]])
+            if not dfs(is_traced, stack, case, _case[0], _case[1]):
+                return False
+            stack.pop()
+    return True
+
+
+def bfs(is_traced, case, i, j):
+    dy, dx = [1,-1,0,0], [0,0,1,-1]
+    queue = deque([(i, j)])
     
-    return [a_place(place) for place in places]
+    while queue:
+        cy, cx = queue.popleft()
+        for d in range(len(dy)):
+            ny, nx = cy + dy[d], cx + dx[d]
+            if not out_of_range([ny, nx], case) and not is_traced[ny][nx]:
+                if case[ny][nx] == "P": return False
+                is_traced[ny][nx] = True
+                if abs(ny - i) + abs(nx - j) <= 1: queue.append((ny, nx))
+    return True
+
+
+def make_is_traced(case):
+    return [[True if case[i][j] == "X" else False 
+            for j in range(len(case[i]))] for i in range(len(case))]
+
+def trace(case):
+    stack = []
+    is_traced = make_is_traced(case)
+    for i in range(len(case)):
+        for j in range(len(case[0])):
+            if case[i][j] == "P":
+                is_traced[i][j] = True
+                result = dfs(is_traced, stack, case, i, j)
+                if not result: return False
+                stack = []
+                is_traced = make_is_traced(case)
+                
+    return True
+
+def bfs_trace(case):
+    is_traced = make_is_traced(case)
+    for i in range(len(case)):
+        for j in range(len(case[0])):
+            if case[i][j] == "P":
+                is_traced[i][j] = True
+                result = bfs(is_traced, case, i, j)
+                if not result: return False
+                is_traced = make_is_traced(case)
+    return True
+
+def solution(places):
+    answers = []
+    
+    for case in places:
+        if bfs_trace(case): answers.append(1)
+        else: answers.append(0)
+    return answers
+
+if __name__ == "__main__":
+  places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+  print(solution(places))
